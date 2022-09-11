7a13107a8953e7ea1347895b4547a4570d94cea7
Fran Swan <fran@boringprotocol.io>
2022-09-11 09:21:35 -0700

    moooooo

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Layout from "../components/layout";
+
+export default function IndexPage() {
+  return (
+    <Layout>
+     yo brosephs, check out this webpage. try to connect with phantom and once connected, go to each page and back and then even try to refresh the browser and see if the wallet connection persists. it should. you'll need to approve twice in phantom. also phantom should pop up autmoatically when you hit the site for the first time. let me know if all this crap works.
+    </Layout>
+  );
+}
