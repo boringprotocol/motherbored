fa359fbae68cd9eb1e630b73ccc1158b05a6a982
Fran Swan <fran@boringprotocol.io>
2022-09-18 08:33:21 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
-import { getSession, useSession } from "next-auth/react";
+import { getSession, signOut, useSession } from "next-auth/react";
+import Router from "next/router";
 import Layout from "../components/layout";
 import React from "react";
 import { GetServerSideProps } from "next";
@@ -14,7 +15,7 @@ import { CheckCircleIcon, XMarkIcon } from '@heroicons/react/20/solid'
 
 const stats = [
   { name: 'Consumers', stat: '54' },
-  { name: 'Providers', stat: '32' },
+  { name: 'Providers', stat: '30' },
   { name: 'Connections', stat: '48' },
 ]
 
@@ -99,9 +100,9 @@ const IndexPage: React.FC<Props> = (props) => {
       <h3 className="text-lg font-medium leading-6 text-gray dark:text-boring-white uppercase">Network</h3>
       <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
         {stats.map((item) => (
-          <div key={item.name} className="overflow-hidden rounded-sm text-boring-black dark:text-boring-white bg-boring-white dark:bg-boring-black px-4 py-5 shadow sm:p-6">
-            <dt className="truncate text-sm font-medium text-gray-500">{item.name}</dt>
-            <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">{item.stat}</dd>
+          <div key={item.name} className="overflow-hidden rounded-sm text-boring-black dark:text-boring-white bg-boring-white dark:bg-boring-black px-4 py-5 shadow-xl sm:p-6">
+            <dt className="truncate text-sm text-gray-500">{item.name}</dt>
+            <dd className="mt-1 text-3xl text-gray">{item.stat}</dd>
           </div>
         ))}
       </dl>
@@ -168,14 +169,76 @@ const IndexPage: React.FC<Props> = (props) => {
       </Head>
 
       <div className="">
-        {props.peers.map((peer) => (
+        
 
-          <div key={peer.id} className="peer">
-            <Peer peer={peer} />
+      <div className="flex border rounded-sm border-gray-light dark:border-gray-dark p-4 mb-12">
+        <div className="flex-shrink-0">
+          <CheckCircleIcon className="h-5 w-5 text-boring-black dark:text-boring-white" aria-hidden="true" />
+        </div>
+        <div className="ml-3">
+          <p className="font-jetbrains text-sm font-medium text-boring-black dark:text-boring-white">Successfully did something</p>
+        </div>
+        <div className="ml-auto pl-3">
+          <div className="-mx-1.5 -my-1.5">
+            <button
+              type="button"
+              className="inline-flex rounded-md bg-green-50 p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-green-50"
+            >
+              <span className="sr-only">Dismiss</span>
+              <XMarkIcon className="h-5 w-5" aria-hidden="true" />
+            </button>
           </div>
-
-        ))}
+        </div>
       </div>
+
+        
+      <ul role="list" className="pb-12 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-2">
+        {props.peers.map((peer) => (
+            <Peer peer={peer} />
+        ))}
+      </ul>
+
+    <div className="pb-12">
+      
+      <button
+    type="button"
+    onClick={() => Router.push("/newpeer")}
+    className="relative block w-full rounded-lg text-boring-black dark:text-boring-white border-2 border-dotted border-gray-light dark:border-gray-dark p-12 text-center hover:border-gray focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+  >
+    <svg
+      className="mx-auto h-12 w-12 text-gray-400"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="currentColor"
+      fill="none"
+      viewBox="0 0 48 48"
+      aria-hidden="true"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M8 14v20c0 4.418 7.163 8 16 8 1.381 0 2.721-.087 4-.252M8 14c0 4.418 7.163 8 16 8s16-3.582 16-8M8 14c0-4.418 7.163-8 16-8s16 3.582 16 8m0 0v14m0-4c0 4.418-7.163 8-16 8S8 28.418 8 24m32 10v6m0 0v6m0-6h6m-6 0h-6"
+      />
+    </svg>
+    <span className="font-jetbrains mt-2 block text-sm font-medium text-gray-900">Add Motherbored</span>
+    
+      </button>
+    </div>
+
+    <div className="pb-12">
+      <h3 className="font-jetbrains text-lg font-medium leading-6 text-gray dark:text-boring-white uppercase">Network</h3>
+      <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+        {stats.map((item) => (
+          <div key={item.name} className="font-jetbrains  overflow-hidden rounded-sm text-boring-black dark:text-boring-white bg-boring-white dark:bg-boring-black px-4 py-5 shadow sm:p-6">
+            <dt className="truncate text-sm font-medium text-gray-500">{item.name}</dt>
+            <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">{item.stat}</dd>
+          </div>
+        ))}
+      </dl>
+      </div>  
+    
+      </div>
+      
     </Layout>
   );
 }
