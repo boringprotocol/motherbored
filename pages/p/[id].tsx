3c3d9f3303d9c6791fe5dc0b5a8597db71248c94
Jeremy Deininger <jeremy@banyansecurity.io>
2022-09-19 15:55:47 -0700

    adding simple configurator for boring.network

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -57,19 +57,37 @@ async function downloadPeerConfig(id: string): Promise<void> {
     await Router.push(`/api/config/${id}`);
 }
 
+async function shovePeerConfig(id: string): Promise<void> {
+    const results = await fetch(`/api/config/${id}`, {
+        method: "GET",
+    });
+    if (results.ok) {
+        const text = await results.text();
+        const sendItURI = "http://boring.network/api/hello?falconconfig=" + encodeURIComponent(text);
+        const sendIt = await fetch(sendItURI, {
+            method: "GET",
+            mode: "no-cors",
+        });
 
+        if (sendIt.ok) {
+            console.log("WE did it, boring.network is configured, please reboot")
+        } else {
+            console.log("something went wrong trying to configure boring.network")
+        }
+    }
+}
 
 const ShowPeer: React.FC<Props> = (props) => {
     // here dude - update components/Peer.tsx to make mroe of these babies
     const [name, setName] = useState(props.peer.name);
     const [label, setLabel] = useState(props.peer.label);
     const [ssid, setSSID] = useState(props.peer.ssid);
-    const id=props.peer.id;
+    const id = props.peer.id;
 
     const submitData = async (e: React.SyntheticEvent) => {
-        console.log (label)
+        console.log(label)
         e.preventDefault();
-        
+
         try {
             const body = { id: id, name: name, label: label, ssid: ssid };
             const response = await fetch(`/api/peer/${id}`, {
@@ -81,7 +99,7 @@ const ShowPeer: React.FC<Props> = (props) => {
             console.log(resultData);
             if (response.ok) {
                 await Router.push(`/p/${resultData.id}`);
-    
+
             } else {
                 await Router.push("/")
             }
@@ -89,7 +107,7 @@ const ShowPeer: React.FC<Props> = (props) => {
             console.error(error);
         }
     };
-    
+
     const { data: session, status } = useSession();
     if (status === "loading") {
         return <div>Authenticating ...</div>;
@@ -113,114 +131,115 @@ const ShowPeer: React.FC<Props> = (props) => {
         <Layout>
             <div>
 
-            {/* <div className="flex">
+                {/* <div className="flex">
   <div className="w-1/2 outline ">w-1/2</div>
   <div className="w-1/2 outline ">w-1/2</div>
 </div> */}
 
-            {/* The Current Peer */}
-            <div className="grid grid-cols-2 gap-4 px-14 py-24 border-b border-gray-light dark:border-gray-dark">
-            <div className="w-3/4">
+                {/* The Current Peer */}
+                <div className="grid grid-cols-2 gap-4 px-14 py-24 border-b border-gray-light dark:border-gray-dark">
+                    <div className="w-3/4">
 
-                <form className="w-full" onSubmit={submitData}>
+                        <form className="w-full" onSubmit={submitData}>
 
-                    <div className="text-boring-white rounded-md px-3 py-2 shadow-sm focus-within:border-blue focus-within:ring-1 focus-within:ring-blue">
-                    {/* <label htmlFor="name" className="block text-xs font-medium text-gray-900">
+                            <div className="text-boring-white rounded-md px-3 py-2 shadow-sm focus-within:border-blue focus-within:ring-1 focus-within:ring-blue">
+                                {/* <label htmlFor="name" className="block text-xs font-medium text-gray-900">
                     Name
                     </label> */}
-                    <input
-                        type="text"
-                        name="name"
-                        id="name"
-                        onChange={(e) => setName(e.target.value)}
-                        className="cursor-not-allowed block w-full border-0 p-0 bg-boring-white dark:bg-boring-black text-boring-black dark:text-boring-white placeholder-boring-black dark:placeholder-boring-white focus:ring-0 text-4xl"
-                        placeholder={name || ""}
-                        disabled
-                    />
+                                <input
+                                    type="text"
+                                    name="name"
+                                    id="name"
+                                    onChange={(e) => setName(e.target.value)}
+                                    className="cursor-not-allowed block w-full border-0 p-0 bg-boring-white dark:bg-boring-black text-boring-black dark:text-boring-white placeholder-boring-black dark:placeholder-boring-white focus:ring-0 text-4xl"
+                                    placeholder={name || ""}
+                                    disabled
+                                />
+                            </div>
+
+                            <div className="bg-boring-white dark:bg-boring-black text-boring-black dark:text-boring-white placeholder-boring-black dark:placeholder-boring-white border border-gray-lightest dark:border-gray-dark rounded-md px-3 py-2 shadow-sm focus-within:border-blue focus-within:ring-1 focus-within:ring-blue">
+                                <label htmlFor="name" className="block text-xs font-medium text-gray-900">
+                                    Label
+                                </label>
+                                <input
+                                    type="text"
+                                    name="label"
+                                    id="label"
+                                    onChange={(e) => setLabel(e.target.value)}
+                                    className="bg-boring-white dark:bg-boring-black text-boring-black dark:text-boring-white placeholder-boring-black dark:placeholder-boring-white block w-full border-0 p-0 focus:ring-0 text-lg"
+                                    placeholder={label || ""}
+                                />
+                            </div>
+
+                            <div className="bg-boring-white dark:bg-boring-black text-boring-black dark:text-boring-white placeholder-boring-black dark:placeholder-boring-white border border-gray-lightest dark:border-gray-dark rounded-md px-3 py-2 shadow-sm focus-within:border-blue focus-within:ring-1 focus-within:ring-blue mt-4">
+                                <label htmlFor="name" className="block text-xs font-medium text-gray-900">
+                                    <IoWifiOutline className="float-left mr-2" /> SSID
+                                </label>
+                                <input
+                                    type="text"
+                                    name="ssid"
+                                    id="ssid"
+                                    onChange={(e) => setSSID(e.target.value)}
+                                    className="bg-boring-white dark:bg-boring-black text-boring-black dark:text-boring-white placeholder-boring-black dark:placeholder-boring-white block w-full border-0 p-0 focus:ring-0 text-lg"
+                                    placeholder={ssid || ""}
+                                />
+                            </div>
+
+                            <button
+                                type="submit"
+                                className="mt-6 flex justify-center rounded-sm border text-boring-black dark:text-boring-white border-boring-black dark:border-boring-white  py-2 px-4 text-sm shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 w-40"
+                                onClick={() => Router.push("/")}
+                            >
+                                Save Changes
+                            </button>
+
+                        </form>
+
                     </div>
 
-                    <div className="bg-boring-white dark:bg-boring-black text-boring-black dark:text-boring-white placeholder-boring-black dark:placeholder-boring-white border border-gray-lightest dark:border-gray-dark rounded-md px-3 py-2 shadow-sm focus-within:border-blue focus-within:ring-1 focus-within:ring-blue">
-                    <label htmlFor="name" className="block text-xs font-medium text-gray-900">
-                    Label
-                    </label>
-                    <input
-                        type="text"
-                        name="label"
-                        id="label"
-                        onChange={(e) => setLabel(e.target.value)}
-                        className="bg-boring-white dark:bg-boring-black text-boring-black dark:text-boring-white placeholder-boring-black dark:placeholder-boring-white block w-full border-0 p-0 focus:ring-0 text-lg"
-                        placeholder={label || ""}
-                    />
-                    </div>
-
-                    <div className="bg-boring-white dark:bg-boring-black text-boring-black dark:text-boring-white placeholder-boring-black dark:placeholder-boring-white border border-gray-lightest dark:border-gray-dark rounded-md px-3 py-2 shadow-sm focus-within:border-blue focus-within:ring-1 focus-within:ring-blue mt-4">
-                    <label htmlFor="name" className="block text-xs font-medium text-gray-900">
-                    <IoWifiOutline className="float-left mr-2" /> SSID
-                    </label>
-                    <input
-                        type="text"
-                        name="ssid"
-                        id="ssid"
-                        onChange={(e) => setSSID(e.target.value)}
-                        className="bg-boring-white dark:bg-boring-black text-boring-black dark:text-boring-white placeholder-boring-black dark:placeholder-boring-white block w-full border-0 p-0 focus:ring-0 text-lg"
-                        placeholder={ssid || ""}
-                    />
-                    </div>
-
-                    <button
-                        type="submit"
-                        className="mt-6 flex justify-center rounded-sm border text-boring-black dark:text-boring-white border-boring-black dark:border-boring-white  py-2 px-4 text-sm shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 w-40"
-                        onClick={() => Router.push("/")}
-                        >
-                        Save Changes
-                    </button>
-
-                </form>
-
-            </div>
-
-            <div className="w-1/4">
-                <Image src={"https://source.boringavatars.com/sunset/" + name || "" + "?colors=264653,2a9d8f,e9c46a,f4a261,e76f51"} alt="" width="48" height="48" /></div>
-                <p className="text-xs" >You are running this peer in <span className="text-gray underline">{props.peer.kind}</span> mode and are connected to <span className="text-gray underline">{props.target}</span></p>
-            </div>
+                    <div className="w-1/4">
+                        <Image src={"https://source.boringavatars.com/sunset/" + name || "" + "?colors=264653,2a9d8f,e9c46a,f4a261,e76f51"} alt="" width="48" height="48" /></div>
+                    <p>You are running this peer in <span className="text-gray underline">{props.peer.kind}</span> mode and are connected to <span className="text-gray underline">{props.target}</span></p>
+                </div>
 
 
                 {/* Advanced Configuration / Settings */}
                 <div className="px-14 py-16 border-b border-gray-light dark:border-gray-dark">
-                <h1 className="font-jetbrains text-2xl">Advanced Configuration</h1>
-                <p className="text-sm">For peers that are not on your local network. </p>
+                    <h1 className="font-jetbrains text-2xl">Advanced Configuration</h1>
+                    <p className="text-sm">For peers that are not on your local network. </p>
 
-                <div className="flex">
-                    
-                    <div className="w-1/2  ">
-                    <ul className="mt-6 text-xs">
-                        <li>Name: {props.peer.name}</li>
-                        <li key={props.peer.id}>Id: {props.peer.id}</li>
-                        <li key={props.peer.kind}>kind: {props.peer.kind}</li>
-                        <li>SSID:</li>
-                        <li key={props.peer.setupkey}>boring setupkey: {props.peer.setupkey}</li>
-                        <li key={props.peer.target}>target: {props.target}</li>
-                    </ul>
-                    </div>
+                    <div className="flex">
 
-                    <div className="w-1/2  ">
-                    <pre className="mt-6 text-sm">ssh 10.0.0.XX:XXXX</pre>
-                    <ul className="mt-6 text-xs">
-                        {props.peer.pubkey && (<li key={props.peer.pubkey}>pubkey: {props.peer.pubkey}</li>)}
-                    </ul>
-                    <button className="mt-8 inline-flex items-center rounded-sm border border-transparent text-xs bg-white px-3 py-2 text-boring-black shadow hover:bg-boring-white" onClick={() => downloadPeerConfig(props.peer.id)}><IoDownloadOutline className="mr-2"/> boring.env</button>
-                    {isProvider && !providerActive && (
-                    <div>
-                        <button className="mt-8 inline-flex items-center rounded-sm border border-transparent bg-white px-3 py-1 text-boring-black shadow hover:bg-gray-lightest" onClick={() => activatePeer(props.peer.id)}>Activate</button>
+                        <div className="w-1/2  ">
+                            <ul className="mt-6 text-xs">
+                                <li>Name: {props.peer.name}</li>
+                                <li key={props.peer.id}>Id: {props.peer.id}</li>
+                                <li key={props.peer.kind}>kind: {props.peer.kind}</li>
+                                <li>SSID:</li>
+                                <li key={props.peer.setupkey}>boring setupkey: {props.peer.setupkey}</li>
+                                <li key={props.peer.target}>target: {props.target}</li>
+                            </ul>
+                        </div>
+
+                        <div className="w-1/2  ">
+                            <pre className="mt-6 text-sm">ssh 10.0.0.XX:XXXX</pre>
+                            <ul className="mt-6 text-xs">
+                                {props.peer.pubkey && (<li key={props.peer.pubkey}>pubkey: {props.peer.pubkey}</li>)}
+                            </ul>
+                            <button className="mt-8 inline-flex items-center rounded-sm border border-transparent text-xs bg-white px-3 py-2 text-boring-black shadow hover:bg-boring-white" onClick={() => downloadPeerConfig(props.peer.id)}><IoDownloadOutline className="mr-2" /> boring.env</button>
+                            <button className="mt-8 inline-flex items-center rounded-sm border border-transparent text-xs bg-white px-3 py-2 text-boring-black shadow hover:bg-boring-white" onClick={() => shovePeerConfig(props.peer.id)}><IoDownloadOutline className="mr-2" /> install config</button>
+                            {isProvider && !providerActive && (
+                                <div>
+                                    <button className="mt-8 inline-flex items-center rounded-sm border border-transparent bg-white px-3 py-1 text-boring-black shadow hover:bg-gray-lightest" onClick={() => activatePeer(props.peer.id)}>Activate</button>
+                                </div>
+                            )}
+                            {isProvider && providerActive && (
+                                <div>
+                                    <h1>This provider is active and available to the network!</h1>
+                                </div>
+                            )}
+                        </div>
                     </div>
-                )}
-                {isProvider && providerActive && (
-                    <div>
-                        <h1>This provider is active and available to the network!</h1>
-                    </div>
-                )}
-                    </div>
-                </div>
 
                 </div>
 
@@ -230,27 +249,27 @@ const ShowPeer: React.FC<Props> = (props) => {
 
                 {/* Advanced Configuration / Settings */}
                 <div className="px-14 py-16 border-b border-gray-light dark:border-gray-dark">
-                
-<h1 className="font-jetbrains text-2xl mt-24">Danger Zone</h1>
 
-    <div className="text-boring-black dark:text-boring-white bg-boring-white dark:bg-boring-black border border-gray-lightest shadow sm:rounded-lg mt-6">
-      <div className="px-4 py-5 sm:p-6">
-        <h3 className="text-lg font-medium ">Destroy Peer</h3>
-        <div className="mt-2 max-w-xl text-sm ">
-          <p>Once you reset your peer, all data associated with it goes away, forever.</p>
-        </div>
-        <div className="mt-5">
-          <button
-            type="button"
-            className="mt-6 flex justify-center rounded-sm border text-boring-black dark:text-boring-white border-boring-black dark:border-boring-white  py-2 px-4 text-sm shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 w-40"
-          >
-            Reset peer
-          </button>
+                    <h1 className="font-jetbrains text-2xl mt-24">Danger Zone</h1>
 
-        </div>
-        </div>
-        </div>
-    </div>    
+                    <div className="text-boring-black dark:text-boring-white bg-boring-white dark:bg-boring-black border border-gray-lightest shadow sm:rounded-lg mt-6">
+                        <div className="px-4 py-5 sm:p-6">
+                            <h3 className="text-lg font-medium ">Destroy Peer</h3>
+                            <div className="mt-2 max-w-xl text-sm ">
+                                <p>Once you reset your peer, all data associated with it goes away, forever.</p>
+                            </div>
+                            <div className="mt-5">
+                                <button
+                                    type="button"
+                                    className="mt-6 flex justify-center rounded-sm border text-boring-black dark:text-boring-white border-boring-black dark:border-boring-white  py-2 px-4 text-sm shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 w-40"
+                                >
+                                    Reset peer
+                                </button>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
 
             </div>
