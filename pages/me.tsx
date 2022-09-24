23eba4c2fc71e9d571c61918d060fdc052981140
Fran Swan <fran@boringprotocol.io>
2022-09-24 13:15:12 -0700

    Update me.tsx

--- pages/me.tsx
+++ pages/me.tsx
@@ -7,6 +7,23 @@ export default function MePage() {
 
   return (
     <Layout>
+      <div className="px-12 py-12">
+      <span className="font-jetbrains text-xs truncate">
+                <IoWalletOutline />
+                {session.user.email ?? session.user.name}
+              </span>
+
+
+{/* // 
+
+lock a consumer to a particluar node yes/no
+You will be able to access the internet only when connected to this peer (VPN). 
+killswitch - don't connect to another network if the provider node is aborted 
+
+*/}
+
+
+      </div>
       {!session && (<pre>not authenticated</pre>)}
       {session?.user && (<pre>{JSON.stringify(data, null, 2)}</pre>)}
     </Layout>
