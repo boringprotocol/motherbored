c72798027be79fa7eb90123aeebad57dadd3c376
Fran Swan <fran@boringprotocol.io>
2022-09-24 13:14:59 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,13 @@
-import { getSession, useSession } from "next-auth/react"
-import Router from "next/router"
-import Layout from "../components/layout"
-import React from "react"
-import { GetServerSideProps } from "next"
-import Peer, { PeerProps } from "../components/Peer"
-import prisma from "../lib/prisma"
-import Head from 'next/head';
-import Image from 'next/image';
-import { CheckCircleIcon, XMarkIcon } from '@heroicons/react/20/solid'
-import { IoWarningOutline } from 'react-icons/io5';
+import { getSession, useSession } from 'next-auth/react'
+import Router from 'next/router'
+import Layout from '../components/layout'
+import React from 'react'
+import { GetServerSideProps } from 'next'
+import Peer, { PeerProps } from '../components/Peer'
+import prisma from '../lib/prisma'
+import Head from 'next/head'
 
-
-// Placeholder data for peer stats
+// Placeholder data for peers stats
 const stats = [
   { name: 'Consumers', stat: '54' },
   { name: 'Providers', stat: '30' },
@@ -55,7 +51,7 @@ type Props = {
 
 const IndexPage: React.FC<Props> = (props) => {
 
-  const { data } = useSession(); // do we need this? 
+  const { data } = useSession(); // do we need this here? 
   const { data: session, status } = useSession();
   if (!session) {
     return (
@@ -66,13 +62,9 @@ const IndexPage: React.FC<Props> = (props) => {
           <title>Boring Protocol</title>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         </Head>
-
         <div className="bg-boring-white dark:bg-boring-black font-jetbrains">
-
-
-
+          login pls
         </div>
-
       </Layout>
     );
   }
@@ -84,15 +76,14 @@ const IndexPage: React.FC<Props> = (props) => {
         <title>Motherbored</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-
+      {/* Main content */}
       <div className="main pt-12">
           
         {/* PEERS */}
-        {/* key={peer.name} has to be on the imideiate child element within a loop
+        {/* key={peer.name} has to be on the first child element within a loop
         https://adhithiravi.medium.com/why-do-i-need-keys-in-react-lists-dbb522188bbb
         https://stackoverflow.com/questions/54401481/eslint-missing-key-prop-for-element-in-iterator-react-jsx-key */}
         <div className="px-14 pb-16">
-        <h2 className="font-jetbrains uppercase mb-4">Peers</h2>
         <ul role="list" className="pb-12 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-2">
           {props.peers.map((peer) => (
             <li key={peer.name} className="col-span-1 border rounded-sm cursor-pointer hover:border-gray dark:hover:border-gray border-gray-lightest dark:border-gray-dark text-boring-black dark:text-boring-white bg-boring-white dark:bg-boring-black ">
@@ -101,39 +92,65 @@ const IndexPage: React.FC<Props> = (props) => {
           ))}
         </ul>
 
+
+
+              
                 {/* ADD PEER */}
-                <div className="pb-12">
-          
-          <button
-        type="button"
-        onClick={() => Router.push("/newpeer")}
-        className="relative block w-full rounded-lg text-boring-black dark:text-boring-white border-2 border-dotted border-gray-light dark:border-gray-dark p-12 text-center hover:border-gray dark:hover:border-gray focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-      >
-        <svg
-          className="mx-auto h-12 w-12 text-gray-400"
-          xmlns="http://www.w3.org/2000/svg"
-          stroke="currentColor"
-          fill="none"
-          viewBox="0 0 48 48"
-          aria-hidden="true"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={1}
-            d="M8 14v20c0 4.418 7.163 8 16 8 1.381 0 2.721-.087 4-.252M8 14c0 4.418 7.163 8 16 8s16-3.582 16-8M8 14c0-4.418 7.163-8 16-8s16 3.582 16 8m0 0v14m0-4c0 4.418-7.163 8-16 8S8 28.418 8 24m32 10v6m0 0v6m0-6h6m-6 0h-6"
-          />
-        </svg>
-        <span className="font-jetbrains mt-2 block text-sm font-medium text-gray-900">Add Peer</span>
-        
-          </button>
-        </div>
-        </div>
-      
+                <div id="add-peer" className="pb-12">
+                <button
+                type="button"
+                onClick={() => Router.push("/newpeer")}
+                className="relative block w-full rounded-lg text-boring-black dark:text-boring-white border-2 border-dotted border-gray-light dark:border-gray-dark p-12 text-center hover:border-gray dark:hover:border-gray focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                <svg
+                  className="mx-auto h-12 w-12 text-gray-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  stroke="currentColor"
+                  fill="none"
+                  viewBox="0 0 48 48"
+                  aria-hidden="true"
+                  >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={1}
+                    d="M8 14v20c0 4.418 7.163 8 16 8 1.381 0 2.721-.087 4-.252M8 14c0 4.418 7.163 8 16 8s16-3.582 16-8M8 14c0-4.418 7.163-8 16-8s16 3.582 16 8m0 0v14m0-4c0 4.418-7.163 8-16 8S8 28.418 8 24m32 10v6m0 0v6m0-6h6m-6 0h-6"
+                  />
+                </svg>
+                <span className="font-jetbrains mt-2 block text-sm font-medium text-gray-900">Add Boring Peer</span>
+                </button>
+                </div>{/* /#add-peer */}
 
-
+                {/* ADD HELIUM HOTSPOT */}
+                <div id="add-peer" className="pb-12">
+                <button
+                disabled
+                type="button"
+                onClick={() => Router.push("/newpeer")}
+                className="cursor-not-allowed relative block w-full rounded-lg text-boring-black dark:text-boring-white border-2 border-dotted border-gray-light dark:border-gray-dark p-12 text-center hover:border-gray dark:hover:border-gray focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                <svg
+                  className="mx-auto h-12 w-12 text-gray-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  stroke="currentColor"
+                  fill="none"
+                  viewBox="0 0 48 48"
+                  aria-hidden="true"
+                  >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={1}
+                    d="M8 14v20c0 4.418 7.163 8 16 8 1.381 0 2.721-.087 4-.252M8 14c0 4.418 7.163 8 16 8s16-3.582 16-8M8 14c0-4.418 7.163-8 16-8s16 3.582 16 8m0 0v14m0-4c0 4.418-7.163 8-16 8S8 28.418 8 24m32 10v6m0 0v6m0-6h6m-6 0h-6"
+                  />
+                </svg>
+                <span className="font-jetbrains mt-2 block text-sm font-medium text-gray-900">Add Helium Hotspot</span>
+                </button>
+                </div>{/* /#add-peer */}
+              
         
-        
+      </div>{/* end .main */}
+  
         
         {/* NETWORK STATISTICS */}
         <div className="px-14 py-16 border-t border-gray-light dark:border-gray-dark">
@@ -149,7 +166,6 @@ const IndexPage: React.FC<Props> = (props) => {
         </div>  
     
       </div>{/* end main */}
-      
     </Layout>
   );
 }
