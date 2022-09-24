4fd4c243cd00f3feddc419c7da86b54a6144e315
Fran Swan <fran@boringprotocol.io>
2022-09-24 13:14:56 -0700

    Update Peer.tsx

--- components/Peer.tsx
+++ components/Peer.tsx
@@ -1,9 +1,7 @@
-import React from "react";
-import Router from "next/router";
-import { IoWifiOutline, IoServerOutline } from 'react-icons/io5';
-import Image from 'next/image';
-
-
+import React from 'react'
+import Router from 'next/router'
+import { IoWifiOutline, IoServerOutline } from 'react-icons/io5'
+import Image from 'next/image'
 
 export type PeerProps = {
   id: string;
@@ -20,8 +18,7 @@ export type PeerProps = {
 };
 
 const Peer: React.FC<{ peer: PeerProps }> = ({ peer }) => {
-  const peerAvatar = "https://source.boringavatars.com/sunset/" + peer.name + "?colors=264653,2a9d8f,e9c46a,f4a261,e76f51"
-
+const peerAvatar = "https://source.boringavatars.com/sunset/" + peer.name
 
   // 
   let providerActive = false
@@ -37,7 +34,7 @@ const Peer: React.FC<{ peer: PeerProps }> = ({ peer }) => {
 
   return (
 
-
+<>
     <a className="border-boring-black hover:border-gray" onClick={() => Router.push("/p/[id]", `/p/${peer.id}`)}>
 
       <div className="flex w-full items-center justify-between space-x-6 p-6">
@@ -46,41 +43,32 @@ const Peer: React.FC<{ peer: PeerProps }> = ({ peer }) => {
           <div className="flex items-center space-x-3">
 
             <h3 className="font-jetbrains truncate text-xl text-boring-black dark:text-boring-white">{peer.name}</h3>
+              <div className="flex items-center space-x-3">
+              
+                <h3 className="font-jetbrains truncate text-xl text-boring-black dark:text-boring-white">{peer.name}</h3>
+              </div>
+              
+              <p className="inline-flex items-center font-jetbrains mt-1 truncate text-sm text-gray-500 capitalize"><IoServerOutline className="float-left mr-2" />{peer.kind} Mode</p>
+
+              <p className="text-xs mt-6 pt-2 border-t border-gray-lightest dark:border-gray-dark"><IoWifiOutline className="float-left mr-3 text-xs"/>{peer.ssid} &middot; {peer.label}</p>
+
+            </div>
+            <Image className="" src={peerAvatar} alt="" width="48" height="48" />
+            
+            <div>{ isProvider && providerActive && ("active")}</div>
+            
           </div>
           <p className="font-jetbrains mt-1 truncate text-sm text-gray-500 capitalize"><IoServerOutline className="float-left mr-2" />{peer.kind} Mode</p>
           <p className="text-xs mt-6 pt-2 border-t border-gray-lightest dark:border-gray-dark"><IoWifiOutline className="float-left mr-3 text-xs" />{peer.ssid} &middot; {peer.label}</p>
-        </div>
-        <Image className="animate-pulse" src={peerAvatar} alt="" width="48" height="48" />
+        
+        <Image className="" src={peerAvatar} alt="" width="48" height="48" />
 
         <div>{isProvider && providerActive && ("active")}</div>
 
       </div>
 
-      {/* 
-        <div key={peer.name} className="font-jetbrains rounded-lg-gray pb-10">
-          <div className="flex w-full items-center justify-between space-x-6 p-6">
-            <div className="flex-1 truncate">
-              <div className="flex items-center space-x-3">
-                <h3 className="truncate text-lg text-gray-900">{peer.name}</h3>
-              </div>
-              <p className="mt-1  text-sm text-gray-500 capitalize">Mode: {peer.kind}</p>
-              <p className="mt-1  text-sm text-gray-500">Group: All, Narnia</p>
-              <p className="mt-1  text-sm text-gray-500">Provided by: {peer.target}</p>
-              <p className="mt-1  text-sm text-gray-500">NFT License: MBV1-1000</p>
-            </div>
-            <img className="h-10 w-10 flex-shrink-0 rounded-full bg-gray-300" src={peerAvatar} alt="" />
-          </div>
-          <div className="px-6  text-xs text-gray-dark">
-            <span className="float-left mr-2"><IoIosKey /></span>
-            <span className="float-left lowercase text-xs">{peer.setupkey} </span>
-          </div>
-          <div>
-          </div>
-        </div> */}
-
-
     </a>
-
+</>
 
   );
 };
