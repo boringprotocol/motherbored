6f34cf977296e2a9a6247e78a131205a34b57149
Fran Swan <fran@boringprotocol.io>
2022-09-24 17:35:07 -0700

    Update Peer.tsx

--- components/Peer.tsx
+++ components/Peer.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Router from 'next/router'
-import { IoWifiOutline, IoServerOutline } from 'react-icons/io5'
+import { IoLaptopOutline, IoWifiOutline, IoServerOutline } from 'react-icons/io5'
 import Image from 'next/image'
 
 export type PeerProps = {
@@ -31,13 +31,29 @@ const peerAvatar = "https://source.boringavatars.com/sunset/" + peer.name
     isProvider = true
   }
 
+  let isConsumer = false
+  if (peer.kind == "consumer") {
+    isConsumer = true
+  }
+
 
   return (
 
 <>
-    <a className="border-boring-black hover:border-gray" onClick={() => Router.push("/p/[id]", `/p/${peer.id}`)}>
+<a className="border-boring-black hover:border-gray" onClick={() => Router.push("/p/[id]", `/p/${peer.id}`)}>
 
-      <div className="flex w-full items-center justify-between space-x-6 p-6">
+<h2 className='text-lg md:text-xl lg:text-2xl px-4 mt-4'>{peer.name}</h2>
+
+<p className="inline-flex items-center font-jetbrains px-4 text-sm text-gray-500 capitalize">
+  {isProvider && (<IoServerOutline className="float-left mr-2" />)}
+  {isConsumer && (<IoLaptopOutline className="float-left mr-2" />)} {peer.kind}
+</p>
+
+<div className='p-4 clear-right'><Image src={peerAvatar} alt="" width="48" height="48" /></div>
+
+<p className="text-xs mt-6 p-4 border-t border-gray-lightest dark:border-gray-dark"><IoWifiOutline className="float-left mr-3 text-xs" />{peer.ssid} &middot; {peer.label}</p>
+
+      {/* <div className="flex w-full items-center justify-between space-x-6 p-6">
         <div className="flex-1 truncate">
 
           <div className="flex items-center space-x-3">
@@ -45,11 +61,11 @@ const peerAvatar = "https://source.boringavatars.com/sunset/" + peer.name
             <h3 className="font-jetbrains truncate text-xl text-boring-black dark:text-boring-white">{peer.name}</h3>
               <div className="flex items-center space-x-3">
               
-                <h3 className="font-jetbrains truncate text-xl text-boring-black dark:text-boring-white">{peer.name}</h3>
+                <h3 className="font-jetbrains truncate text-xl text-boring-black dark:text-boring-white">lkjhlkjh{peer.name}</h3>
               </div>
               
               <p className="inline-flex items-center font-jetbrains mt-1 truncate text-sm text-gray-500 capitalize"><IoServerOutline className="float-left mr-2" />{peer.kind} Mode</p>
-
+              {peer.name}
               <p className="text-xs mt-6 pt-2 border-t border-gray-lightest dark:border-gray-dark"><IoWifiOutline className="float-left mr-3 text-xs"/>{peer.ssid} &middot; {peer.label}</p>
 
             </div>
@@ -65,7 +81,7 @@ const peerAvatar = "https://source.boringavatars.com/sunset/" + peer.name
 
         <div>{isProvider && providerActive && ("active")}</div>
 
-      </div>
+      </div> */}
 
     </a>
 </>
