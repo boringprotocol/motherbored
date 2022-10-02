ac76a5ececb8db8b4c9caf139ac268ea8e0936f4
Fran Swan <fran@boringprotocol.io>
2022-10-01 19:04:13 -0700

    metaplex tests

--- pages/brng.tsx
+++ pages/brng.tsx
@@ -1,28 +1,48 @@
 import { useSession } from "next-auth/react"
 import LayoutAuthenticated from "../components/layoutAuthenticated"
-import { Metaplex, keypairIdentity } from "@metaplex-foundation/js";
-import { Connection, clusterApiUrl, Keypair, PublicKey } from "@solana/web3.js";
+import { useEffect, useState } from "react";
+import { Connection } from "@metaplex/js";
+import { Account } from "@metaplex-foundation/mpl-core";
+import { Metadata } from "@metaplex-foundation/mpl-token-metadata";
 
-(async () => {
-  const connection = new Connection(clusterApiUrl("mainnet-beta"));
-  const keypair = Keypair.generate();
-
-  const metaplex = new Metaplex(connection);
-  metaplex.use(keypairIdentity(keypair));
+import Card from "../components/Card";
 
 
-  // See about some Scoogis
-  const mint = new PublicKey("22bKSRkhQywMVPDguBs1KJu5VkHvrXDFjHcKqJJLKcTu");
-  const nft = await metaplex.nfts().findByMint(mint);
+const connection = new Connection("mainnet-beta");
+const tokenAddress = "CxkKDaBvtHqg8aHBVY8E4YYBsCfJkJVsTAEdTo5k4SEw";
+
+
   
-  // log json formatted metadata
-  console.log(nft.metadata);
-  
-})();
 
 export default function MePage() {
   const { data } = useSession()
   const { data: session, status } = useSession();
+  const [tokenAdr, setToken] = useState(tokenAddress);
+  const [newToken, setNewToken] = useState(tokenAddress);
+  const [setData] = useState<unknown>(null);
+  const [imageSrc, setImage] = useState(null);
+
+  useEffect(() => {
+    const loadImageData = async (uri: string) => {
+      const response = await fetch(uri);
+      const { image } = await response.json();
+      setImage(image);
+    };
+
+    const getMetadata = async () => {
+      const metadataPDA = await Metadata.getPDA(tokenAdr);
+      const mintAccInfo = await connection.getAccountInfo(metadataPDA);
+
+      const {
+        data: { data: metadata }
+      } = Metadata.from(new Account(tokenAddress, mintAccInfo));
+
+      await loadImageData(metadata.uri);
+      setData(metadata);
+    };
+
+    getMetadata();
+  }, [tokenAdr]);
 
   return (
     <LayoutAuthenticated>
@@ -31,9 +51,27 @@ export default function MePage() {
                 hello
               </span>
 
+              <div className="App">
+      <input
+        value={newToken}
+        onChange={(evt) => setNewToken(evt.target.value)}
+      />
+      <button onClick={() => setToken(newToken)}>Load metadata</button>
+      {!data ? (
+        "Loading..."
+      ) : (
+        <div>
+          <Card imageSrc={imageSrc} text={data?.name} />
+          <h2>Metadata</h2>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+      )}
+    </div>
+
       </div>
       {!session && (<pre>not authenticated</pre>)}
       {session?.user && (<pre>{JSON.stringify(data, null, 2)}</pre>)}
     </LayoutAuthenticated>
   )
 }
+
