fa8f0089d36e05a6fd338799699599dff197549d
Fran Swan <fran@boringprotocol.io>
2022-11-26 16:44:08 -0800

    Update layout.tsx

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,13 +1,34 @@
 import { Fragment, useEffect, useState } from 'react'
-import { useTheme } from 'next-themes'
+import { ThemeProvider, useTheme } from 'next-themes'
 import Waiting from './art/waiting'
-import { IoWalletOutline } from 'react-icons/io5'
+import { IoMoonOutline, IoSunnyOutline, IoWalletOutline } from 'react-icons/io5'
 import { getCsrfToken, signIn, useSession } from 'next-auth/react'
 import { SigninMessage } from '../utils/SigninMessage'
 import bs58 from 'bs58'
 import { useWalletModal } from '@solana/wallet-adapter-react-ui'
 import { useWallet } from '@solana/wallet-adapter-react'
 import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
+import PhantomLogo from './art/phantom'
+
+
+// light mode / dark mode
+const ThemeChanger = () => {
+  const { theme, setTheme } = useTheme()
+
+  return (
+    <div className='mt-12 border rounded-sm  border-gray dark:border-black'>
+      <button className='text-center inline-flex items-center  text-xs bg-white px-3 py-2 text-boring-black hover:bg-gray-lightestest dark:hover:bg-gray-light border-r border-gray dark:border-black  w-1/2' onClick={() => setTheme('light')}><IoSunnyOutline className="float-left mr-2" /> Light Mode</button>
+      <button className='inline-flex items-center text-xs bg-white px-3 py-2 text-boring-black hover:bg-gray-lightestest dark:hover:bg-gray-light w-1/2' onClick={() => setTheme('dark')}><IoMoonOutline className="float-left mr-2" /> Dark Mode</button>
+    </div>
+  )
+}
+
+// Placeholder data for peers stats
+const stats = [
+  { name: 'Consumers', stat: '54' },
+  { name: 'Providers', stat: '30' },
+  { name: 'Countries', stat: '48' },
+]
 
 function classNames(...classes: any) {
   return classes.filter(Boolean).join(' ')
@@ -68,35 +89,70 @@ export default function Layout({ children }: Props) {
   return (
 
     <>
+      <div className="font-jetbrains text-boring-black dark:text-boring-white bg-boring-white dark:bg-boring-black">
 
-
-      <div className="flex items-center justify-center h-screen text-white">
         <div className="p-4">
-          <div className='font-jetbrains '>
-          {/* <div className='p-2 text-sm '>Boring Protocol</div> */}
-            {/* <div className='p-2 text-xs'>A Private Path</div> */}
-            <Waiting />
+        <div className='pt-4 text-xs '>A Private Path - motherbored.app - Boring Protocol</div>
+        <ThemeChanger />
+        
+          {/* Network Statitics */}
+          <div className="p-4">
+            Network Stats
+            
+            <div className="">
+            <dl className="mt-5 grid grid-cols-3 gap-5 sm:grid-cols-3">
+              {stats.map((item) => (
+                <div key={item.name} className="font-jetbrains  overflow-hidden  text-boring-black dark:text-boring-white bg-boring-white dark:bg-boring-black py-5">
+                  <dt className="truncate text-sm sm:text-sm">{item.name}</dt>
+                  <dd className="mt-1 text-2xl sm:text-3xl">{item.stat}</dd>
+                </div>
+              ))}
+            </dl>
+            </div>  
           </div>
+          {/* end /Network Statitics */}
+              
+                {/* <Waiting /> */}
+              
 
           <BrowserView>
-            <a href="#" className="m-4 inline-flex items-center rounded-sm border border-transparent text-xs bg-white px-3 py-2 text-boring-black shadow hover:bg-boring-white" onClick={handleSignIn}>
-               <IoWalletOutline className="mr-2"/> Connect Wallet
+          Desktop Options<br></br>
+            <a href="#" className="inline-flex items-center rounded-sm border border-transparent text-xs bg-white px-3 py-2 text-boring-black shadow hover:bg-boring-white" onClick={handleSignIn}>
+               <IoWalletOutline className="mr-2"/> Connect Phantom
+            </a>
+            <a href="#" className="inline-flex items-center rounded-sm border border-transparent text-xs bg-white px-3 py-2 text-boring-black shadow hover:bg-boring-white" onClick={handleSignIn}>
+               <IoWalletOutline className="mr-2"/> Connect Metamask
+            </a>
+
+            <a href="#" className="inline-flex items-center rounded-sm border border-transparent text-xs bg-white px-3 py-2 text-boring-black shadow hover:bg-boring-white" onClick={handleSignIn}>
+               <IoWalletOutline className="mr-2"/> Connect Ledger
             </a>
           </BrowserView>
 
           <MobileView>
-          <a href="#" className="m-4 inline-flex items-center rounded-sm border border-transparent text-xs bg-white px-3 py-2 text-boring-black shadow hover:bg-boring-white" onClick={handleSignIn}>
-               <IoWalletOutline className="mr-2"/> Connect Wallet
+            Mobile Options<br></br>
+
+            <a href="#" className="inline-flex items-center rounded-sm border border-transparent text-xs bg-white px-3 py-2 text-boring-black shadow hover:bg-boring-white" onClick={handleSignIn}>
+               <IoWalletOutline className="mr-2"/> Connect Phantom
+            </a>
+            <a href="#" className="inline-flex items-center rounded-sm border border-transparent text-xs bg-white px-3 py-2 text-boring-black shadow hover:bg-boring-white" onClick={handleSignIn}>
+               <IoWalletOutline className="mr-2"/> Connect Metamask
+            </a>
+
+            <a href="#" className="inline-flex items-center rounded-sm border border-transparent text-xs bg-white px-3 py-2 text-boring-black shadow hover:bg-boring-white" onClick={handleSignIn}>
+               <IoWalletOutline className="mr-2"/> Connect Ledger
             </a>
             {/* <a className="m-4 inline-flex items-center rounded-sm border border-transparent text-xs bg-white px-3 py-2 text-boring-black shadow hover:bg-boring-white" href="https://phantom.app/ul/browse/https%3A%2F%2Fboring-falcon.netlify.app"><IoWalletOutline className="mr-2" />Connect Phantom</a> */}
           </MobileView>
 
 
-        <p className="p-4 text-xs text-boring-black dark:text-gray">iOS and Android users should be able to use the Phantom wallet in-app browser to access this Motherbored GUI.  </p>
-        <div className=''>         
+        <p className="mt-4 text-xs text-boring-black dark:text-gray">iOS and Android users should be able to use the Phantom or Metamask wallet's in-app browsers to access this Motherbored GUI.  </p>
+        <div className='text-boring-black dark:text-boring-white mt-4'>     
+        {/* <PhantomLogo />     */}
+        <a className="border p-2" href="https://docs.motherbored.app" target="_blank">Docs</a> | <a  className="border p-2" href="https://store.motherbored.limited">Store</a>
         </div>
 
-        <p className="text-xs text-gray"></p>
+        <p className="mt-4 text-xs text-gray"><a className="underline" href="http://localhost:3000/directory/">Find a provider</a> to connect to</p>
       </div>
     </div>
     </>
