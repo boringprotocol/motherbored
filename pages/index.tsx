ce0d6c1d17fcf06ec1840bf472b11b7c06badac9
Fran Swan <fran@boringprotocol.io>
2023-01-11 10:40:13 -0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,11 @@ import { getSession, useSession } from 'next-auth/react'
 import Router from 'next/router'
 import Layout from '../components/layout'
 import LayoutAuthenticated from '../components/layoutAuthenticated'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { GetServerSideProps } from 'next'
 import Peer, { PeerProps } from '../components/Peer'
 import prisma from '../lib/prisma'
 import Head from 'next/head'
-
-
-// Placeholder data for peers stats
-// const stats = [
-//   { name: 'Consumers', stat: '54' },
-//   { name: 'Providers', stat: '30' },
-//   { name: 'Connections', stat: '48' },
-// ]
 
 const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
 
@@ -46,8 +38,6 @@ const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
     props: { peers },
   }
 }
-
-
 
 type Props = {
   peers: PeerProps[],
@@ -84,15 +74,8 @@ const IndexPage: React.FC<Props> = (props) => {
       {/* Main content */}
       <div className="main pt-12 text-xs">
 
-        {/* Maybe just add/remove a 'hidden' class on the rendered peer list item. Or probably useState though for back button  */}
-        {/* <div className='p-12'>
-      show peers: <a className="border p-1" href="#">all</a>, providers: <a className="border p-1" href="#">all</a> <a className="border p-1" href="#">local</a> <a className="border p-1" href="#">cloud</a> consumers: <a className="border p-1" href="#">all</a>
-      </div> */}
-
         {/* PEERS */}
-        {/* key={peer.name} has to be on the first child element within a loop
-        https://adhithiravi.medium.com/why-do-i-need-keys-in-react-lists-dbb522188bbb
-        https://stackoverflow.com/questions/54401481/eslint-missing-key-prop-for-element-in-iterator-react-jsx-key */}
+       
         <div className="px-4 sm:px-8 md:px-12 pb-16">
           <ul role="list" className=" pb-12 grid grid-cols-2 gap-6 sm:grid-cols-4 2xl:grid-cols-6">
             <li className="shadow-md rounded-lg pb-2 pt-4 col-span-2 border border-gray-light dark:border-gray-dark hover:border-gray dark:hover:border-gray">
@@ -178,56 +161,9 @@ const IndexPage: React.FC<Props> = (props) => {
                 <span className="font-jetbrains mt-2 block text-xs text-gray">Deploy to a virtual machine offered by any cloud provider (e.g., AWS, DigitalOcean, Hetzner, Google Cloud, Contabo ...)</span>
               </button>
             </li>
-            <li className="shadow-md rounded-lg pb-2 pt-4 col-span-2 border border-gray-light dark:border-gray-dark hover:border-gray dark:hover:border-gray">
-              <button
-                title="Coming Soon"
-                disabled
-                type="button"
-                onClick={() => Router.push("/newpeer")}
-                className="cursor-not-allowed relative block w-full rounded-lg text-boring-black dark:text-boring-white p-12 text-center hover:border-gray dark:hover:border-gray focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              >
-                <svg
-                  className="mx-auto h-12 w-12 text-gray-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  stroke="currentColor"
-                  fill="none"
-                  viewBox="0 0 48 48"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={1}
-                    d="M8 14v20c0 4.418 7.163 8 16 8 1.381 0 2.721-.087 4-.252M8 14c0 4.418 7.163 8 16 8s16-3.582 16-8M8 14c0-4.418 7.163-8 16-8s16 3.582 16 8m0 0v14m0-4c0 4.418-7.163 8-16 8S8 28.418 8 24m32 10v6m0 0v6m0-6h6m-6 0h-6"
-                  />
-                </svg>
-                <span className="font-jetbrains mt-2 block text-sm text-gray-dark dark:text-boring-white">Add Helium Hotspot Provider</span>
-                <span className="font-jetbrains mt-2 block text-xs text-gray">Run a provider peer on network-connected Helium devices. coming soon...</span>
-              </button>
-            </li>
           </ul>
-
-
-
-
-
-        </div>{/* end .main */}
-
-
-        {/* NETWORK STATISTICS */}
-        {/* <div className="px-4 sm:px-8 md:px-12 py-16 border-t border-gray-light dark:border-gray-dark">
-        <h3 className="font-jetbrains text-lg  text-gray dark:text-boring-white uppercase">Network</h3>
-        <dl className="mt-5 grid grid-cols-2 gap-5 sm:grid-cols-3">
-          {stats.map((item) => (
-            <div key={item.name} className="font-jetbrains  overflow-hidden rounded-sm text-boring-black dark:text-boring-white bg-boring-white dark:bg-boring-black py-5 shadow sm:p-6">
-              <dt className="truncate text-sm font-medium text-gray-500">{item.name}</dt>
-              <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">{item.stat}</dd>
-            </div>
-          ))}
-        </dl>
-        </div>   */}
-
-      </div>{/* end main */}
+        </div>
+      </div>
     </LayoutAuthenticated>
   );
 }
