1c0e66988ef7d75f0418bbceaf87ec3be1a11671
Fran Swan <fran@boringprotocol.io>
2023-03-08 03:54:02 -0800

    Update RunAllAccountsRecords.tsx

--- components/accounts-records/RunAllAccountsRecords.tsx
+++ components/accounts-records/RunAllAccountsRecords.tsx
@@ -1,17 +1,26 @@
 import React, { useState } from "react";
-import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { faSpinner, faCheck } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const RunAllAccountsRecords: React.FC = () => {
   const [isUpdating, setIsUpdating] = useState(false);
+  const [isPeersFinished, setIsPeersFinished] = useState(false);
+  const [isSoftStakeFinished, setIsSoftStakeFinished] = useState(false);
+  const [isNFTLicensesFinished, setIsNFTLicensesFinished] = useState(false);
 
   const handleClick = async () => {
     setIsUpdating(true);
+    setIsPeersFinished(false);
+    setIsSoftStakeFinished(false);
+    setIsNFTLicensesFinished(false);
     try {
       const response = await fetch("/api/accounts-records/run-all", {
         method: "POST",
       });
       if (response.ok) {
+        setIsPeersFinished(true);
+        setIsSoftStakeFinished(true);
+        setIsNFTLicensesFinished(true);
         alert("All endpoints completed successfully");
       } else {
         alert("Update failed");
@@ -34,6 +43,17 @@ const RunAllAccountsRecords: React.FC = () => {
       ) : (
         "Run All Accounts Records"
       )}
+      <div className="flex space-x-2 ml-2">
+        {isPeersFinished ? (
+          <FontAwesomeIcon icon={faCheck} className="text-green-500" />
+        ) : null}
+        {isSoftStakeFinished ? (
+          <FontAwesomeIcon icon={faCheck} className="text-green-500" />
+        ) : null}
+        {isNFTLicensesFinished ? (
+          <FontAwesomeIcon icon={faCheck} className="text-green-500" />
+        ) : null}
+      </div>
     </button>
   );
 };
