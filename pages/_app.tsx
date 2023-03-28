89be3ddefbf3dbad0f6e3b27d4d434ae195f39db
Fran Swan <fran@boringprotocol.io>
2023-03-28 10:19:52 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,50 +1,28 @@
-// /pages/_app.tsx
-import { SessionProvider } from "next-auth/react";
-import React, { useMemo } from 'react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { CoinbaseWalletAdapter, PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
-import { clusterApiUrl } from '@solana/web3.js';
-import { ThemeContextProvider } from "../Themes/themeContext";
-import { ClaimContextProvider } from "../contexts/ClaimContext";
-
-import Router from 'next/router';
+// /shit/_app.tsx
 import '../styles/global.css';
-
-// Progress bar
+import { SessionProvider } from "next-auth/react";
+import React, { useMemo } from "react";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { CoinbaseWalletAdapter, PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
+import { ThemeContextProvider } from 'Themes/themeContext';
+import { ClaimContextProvider } from 'contexts/ClaimContext';
+import Router from 'next/router';
 import nProgress from 'nprogress';
 import '../styles/nprogress.css';
 
-// Notifications
-import '../styles/toast.css';
-
-//Progress bar binding events
+//Binding events. 
 Router.events.on("routeChangeStart", nProgress.start);
 Router.events.on("routeChangeError", nProgress.done);
 Router.events.on("routeChangeComplete", nProgress.done);
 
-// require('@solana/wallet-adapter-react-ui/styles.css');
+// Default styles that can be overridden by your app
+// removing this cocksucker will break the modal and nobody gets in
+require('@solana/wallet-adapter-react-ui/styles.css');
 
-// development and production URLs
-const motherboredAppDev = 'http://localhost:3001'; // NODE_ENV=development PORT=3001 npm run dev
-const motherboredAppProd = 'https://motherbored.app'; // NODE_ENV=production PORT=3001 npm run dev
-const boringProtocolDev = 'http://localhost:3002'; // NODE_ENV=development PORT=3002 npm run dev
-const boringProtocolProd = 'https://boringprotocol.io'; // NODE_ENV=production PORT=3002 npm run dev
-const motherboredDocsDev = 'http://localhost:3003'; // NODE_ENV=development PORT=3003 npm run dev
-const motherboredDocsProd = 'https://docs.motherbored.app'; // NODE_ENV=production PORT=3003 npm run dev
-
-// `useSession()` anywhere to access the `session` object.
-export default function Mothebored({ Component, pageProps: { session, ...pageProps } }: any) {
-
-  // PORT=3001 npm run dev
-  const motherboredApp =
-    process.env.NODE_ENV === 'production' ? motherboredAppProd : motherboredAppDev;
-  // PORT = 3003 npm run dev
-  const boringProtocol =
-    process.env.NODE_ENV === 'production' ? boringProtocolProd : boringProtocolDev;
-  // PORT=3003 npm run dev
-  const motherboredDocs = process.env.NODE_ENV === 'production' ? motherboredDocsProd : motherboredDocsDev;
+export default function App({ Component, pageProps: { session, ...pageProps } }: any) {
 
   const network = WalletAdapterNetwork.Mainnet;
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
@@ -55,6 +33,7 @@ export default function Mothebored({ Component, pageProps: { session, ...pagePro
     ],
     []
   );
+
   return (
     <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={wallets} autoConnect>
@@ -63,12 +42,7 @@ export default function Mothebored({ Component, pageProps: { session, ...pagePro
             <ThemeContextProvider>
               <ClaimContextProvider>
                 <div className="min-h-screen">
-                  <Component
-                    {...pageProps}
-                    motherboredApp={motherboredApp}
-                    boringProtocol={boringProtocol}
-                    motherboredDocs={motherboredDocs}
-                  />
+                  <Component {...pageProps} />
                 </div>
               </ClaimContextProvider>
             </ThemeContextProvider>
